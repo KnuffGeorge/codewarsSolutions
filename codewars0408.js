b57352d04e3634a804d70a6412c7b455b7ea46eb
KnuffGeorge <georgethek@gmail.com>
2022-04-09 13:44:24 -0700

    number 5

--- codewars0408.js
+++ codewars0408.js
@@ -65,4 +65,4 @@ function HQ9(code) {
       return ninetyNine + "1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall."
     }
   }
-  
+git  
