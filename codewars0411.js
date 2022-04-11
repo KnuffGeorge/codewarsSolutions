1bf399c8a2d39fc2b95046679b933a8b2355e724
KnuffGeorge <georgethek@gmail.com>
2022-04-11 16:15:08 -0700

    7 kyu lets gooo

--- codewars0411.js
+++ codewars0411.js
@@ -61,3 +61,14 @@ function any(arr, fun){
         return "Rating not recognised"
     }
     }
+
+//     Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+function getCount(str) {
+  let vowels = ['a', 'e', 'i', 'o', 'u'];
+  return str.split('').filter(x => vowels.includes(x)).length
+}
