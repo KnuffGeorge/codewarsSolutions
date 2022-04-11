b19a47361fa92cfeec53e1fb459ffcd7611eb284
KnuffGeorge <georgethek@gmail.com>
2022-04-11 09:37:31 -0700

    any? function question

--- codewars0411.js
+++ codewars0411.js
@@ -23,3 +23,9 @@ function isPalindrome(line) {
 var cubeChecker = function(volume, side){
     return side > 0 ? (side ** 3) == volume : false
   };
+
+//   Create an any? (JS: any) function that accepts an array and a block (JS: function), and returns true if the block (/function) returns true for any item in the array. If the array is empty, the function should return false.
+
+function any(arr, fun){
+    return arr.filter(fun).length > 0 
+  }
