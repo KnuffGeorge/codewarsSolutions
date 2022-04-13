d1c50971f3b3ffce8411b119b4a5945aa2e2834a
KnuffGeorge <georgethek@gmail.com>
2022-04-13 13:52:52 -0700

    longest word

--- codewars0413.js
+++ codewars0413.js
@@ -13,3 +13,15 @@ function isAllPossibilities(x){
       }
     }return x.length == 0 ? false : true
     }
+    
+// Complete the function that takes one argument, a list of words, and returns the length of the longest word in the list.
+
+// For example:
+
+// ['simple', 'is', 'better', 'than', 'complex'] ==> 7
+
+// Do not modify the input list.
+
+function longest(words) {
+    return Math.max(...words.map(x => x.length))
+  }
