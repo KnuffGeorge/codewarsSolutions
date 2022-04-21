8c9e0ff7790f00dd9fced02f697d88e7949031b9
KnuffGeorge <georgethek@gmail.com>
2022-04-20 21:41:01 -0700

    broken counter

--- codewars0420.js
+++ codewars0420.js
@@ -11,3 +11,22 @@ function remove(s,n){
         }
     }return filtered
   }
+
+//   Our counter prototype is broken. Can you spot, what's wrong here?
+
+function Counter(){
+    this.value = 0;
+  
+  
+  Counter.prototype.increase = function() {
+    this.value++;
+  };
+  
+  Counter.prototype.getValue = function() {
+    return this.value;
+  };
+  
+  Counter.prototype.reset = function() {
+    this.value = 0;
+  }
+  }
