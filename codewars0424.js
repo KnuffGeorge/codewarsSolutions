6e153fdfa283df994864720959cebb10a1080c6d
KnuffGeorge <georgethek@gmail.com>
2022-04-24 14:24:42 -0700

    character count

--- codewars0424.js
+++ codewars0424.js
@@ -113,4 +113,20 @@ function findOdd(A) {
   })
     return answer[0]
   }
+
+//   Welcome, Warrior! In this kata, you will get a message and you will need to get the frequency of each and every character!
+// Explanation
+
+// Your function will be called char_freq/charFreq/CharFreq and you will get passed a string, you will then return a dictionary (object in JavaScript) with as keys the characters, and as values how many times that character is in the string. You can assume you will be given valid input. 
+
+function charFreq(message) {
+  let answer = { }
   
+  for (let i = 0; i < message.length; i++){
+    if(message[i] in answer){
+      answer[message[i]] += 1
+  } else {
+      answer[message[i]] = 1
+  }
+  }return answer
+  }
