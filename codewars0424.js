d8482b255f781564a97c1602fe42e405c2ec5c8a
KnuffGeorge <georgethek@gmail.com>
2022-04-24 10:39:51 -0700

    odd integer

--- codewars0424.js
+++ codewars0424.js
@@ -80,3 +80,37 @@ function binToDec(bin){
     return sum
   }
   
+
+//   Given a non-negative integer n, write a function to_binary/ToBinary which returns that number in a binary format.
+
+function toBinary(n){
+    return Number(n.toString(2))
+    
+  }
+
+//   Given an array of integers, find the one that appears an odd number of times.
+
+//   There will always be only one integer that appears an odd number of times.
+//   Examples
+  
+//   [7] should return 7, because it occurs 1 time (which is odd).
+//   [0] should return 0, because it occurs 1 time (which is odd).
+//   [1,1,2] should return 2, because it occurs 1 time (which is odd).
+//   [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+//   [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+function findOdd(A) {
+    let answer = []
+    A.forEach(x => {
+      let count = 0
+      for (let i = 0; i <= A.length; i++){
+        if (A[i] == x){
+          count++
+        }
+        }if (count % 2 != 0) {
+          answer.push(x)
+       }
+  })
+    return answer[0]
+  }
+  
