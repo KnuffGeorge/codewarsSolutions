c353ff448c067eaa05c101195baad96e00a2762d
KnuffGeorge <georgethek@gmail.com>
2022-04-23 20:44:32 -0700

    unique in order

--- codewars0423.js
+++ codewars0423.js
@@ -71,3 +71,19 @@ function solution(number){
     function createPhoneNumber(n){
         return `\(${n[0]}${n[1]}${n[2]}) ${n[3]}${n[4]}${n[5]}-${n[6]}${n[7]}${n[8]}${n[9]}`
       }
+
+// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+// For example:
+
+// uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+// uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+// uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+var uniqueInOrder=function(iterable){
+    let answer = []
+    for (let i = 0; i < iterable.length; i++){
+      iterable[i-1] != iterable[i] ? answer.push(iterable[i]) : null
+    }
+    return answer
+} 
