ca347320be53fc89b1eaceb108e994229b611591
KnuffGeorge <georgethek@gmail.com>
2022-04-24 09:41:52 -0700

    binary loop

--- codewars0424.js
+++ codewars0424.js
@@ -26,3 +26,57 @@ function isDivisible(wallLength, pixelSize){
         return `${this.name}s age is ${this.ageage}`;
     }
   }
+
+//   Everybody knows the classic "half your age plus seven" dating rule that a lot of people follow (including myself). It's the 'recommended' age range in which to date someone.
+
+//   minimum age <= your age <= maximum age #Task
+  
+//   Given an integer (1 <= n <= 100) representing a person's age, return their minimum and maximum age range.
+  
+//   This equation doesn't work when the age <= 14, so use this equation instead:
+  
+//   min = age - 0.10 * age
+//   max = age + 0.10 * age
+  
+//   You should floor all your answers so that an integer is given instead of a float (which doesn't represent age). Return your answer in the form [min]-[max]
+
+function datingRange(age){
+    let min = Math.floor(age / 2 + 7)
+    let max = Math.floor((age - 7) * 2)
+    if (age <= 14){
+      min = Math.floor(age - 0.1 * age)
+      max = Math.floor(age + 0.1 * age)
+    }
+    
+    return min+'-'+max
+  }
+
+//   The function takes in 2 inputs x and y, and should return x to the power of y
+
+// Simple, right? But you're NOT allowed to use Math.pow();
+
+// Obs: x and y will be naturals, so DON'T take fractions into consideration;
+
+// Note : zero to the power of zero equals one in this kata
+
+// Great coding <3
+
+function power(x,y){
+    return x ** y
+  }
+
+
+//   Complete the function which converts a binary number (given as a string) to a decimal number.
+
+function binToDec(bin){
+    let multiplier = 0
+    let place = bin.length
+    let sum = 0
+    for (let i = bin.length; i >= 0; i--){
+     sum += bin.charAt(place) * multiplier
+     multiplier == 0 ? multiplier += 1 : multiplier *= 2
+     place--
+    }
+    return sum
+  }
+  
