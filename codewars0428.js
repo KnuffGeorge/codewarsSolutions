776960562278768e91112b39a15c318d33b01408
KnuffGeorge <georgethek@gmail.com>
2022-04-28 22:18:15 -0700

    squarea

--- codewars0428.js
+++ codewars0428.js
@@ -66,3 +66,9 @@ function remove (string) {
         return answer.reduce((acc, c) => acc + c) == 0 ? [] : answer 
       }
 
+    //   Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
+      function squareArea(A){
+        let answer =  Number((((A * 4) / (2 * Math.PI)) ** 2).toFixed(2))
+        return answer
+        }
+
