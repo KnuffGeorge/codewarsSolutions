72b00f10300f390ff515ac45ecc0d9f7a488a83b
KnuffGeorge <georgethek@gmail.com>
2022-04-28 22:19:26 -0700

    palindrome checker

--- codewars0428.js
+++ codewars0428.js
@@ -72,3 +72,7 @@ function remove (string) {
         return answer
         }
 
+        // Write a function that checks if a given string (case insensitive) is a palindrome.
+        function isPalindrome(x) {
+            return x.toLowerCase() == x.split('').reverse().join('').toLowerCase()
+              }
