7b4043b0cd57ea6366abf36d51060958b4c666b6
KnuffGeorge <georgethek@gmail.com>
2022-05-02 21:29:21 -0700

    csv formatter

--- codewars0502.js
+++ codewars0502.js
@@ -9,3 +9,27 @@
 function evil(n) {
     return n.toString(2).split('').reduce((ac, cu) => ac + +cu, 0) % 2 === 0 ? "It's Evil!" : "It's Odious!"
   }
+
+ 
+//   Create a function that returns the CSV representation of a two-dimensional numeric array.
+
+//   Example:
+  
+//   input:
+//      [[ 0, 1, 2, 3, 4 ],
+//       [ 10,11,12,13,14 ],
+//       [ 20,21,22,23,24 ],
+//       [ 30,31,32,33,34 ]] 
+      
+//   output:
+//        '0,1,2,3,4\n'
+//       +'10,11,12,13,14\n'
+//       +'20,21,22,23,24\n'
+//       +'30,31,32,33,34'
+  
+//   Array's length > 2.
+  function toCsvText(array) {
+    let csv = '' 
+    array.forEach(x => csv += x + "\n")
+    return csv.slice(0, -1)
+  }
