3fd19288637bd05e22bc978562a00692fecc6f6b
KnuffGeorge <georgethek@gmail.com>
2022-05-04 22:17:28 -0700

    pad it

--- codewars0504.js
+++ codewars0504.js
@@ -21,3 +21,56 @@
 function countWords(str) {
     return Math.max(str.trim().split(' ').filter(x => x != '').length, str.trim().split('﻿').filter(x => x != '').length )
   }
+
+//   Coding in function fiveLine, function accept 1 parameter:s. s is a string.
+
+// Please return a string of 5 lines(newline symbol is \n). The first line has one s; Second line have two s; and so on..Fifth line have five s;
+
+// Note1: The two sides of the parameter s may contain some whitespace, please clear them before using s.
+
+// Note2: Using a string template can make your job easier.
+
+// Example:
+
+// fiveLine("  a") should return "a\naa\naaa\naaaa\naaaaa"
+// a
+// aa
+// aaa
+// aaaa
+// aaaaa       <---The effect when you console.log it
+// fiveLine("  xy ") 
+// should return "xy\nxyxy\nxyxyxy\nxyxyxyxy\nxyxyxyxyxy"
+// xy
+// xyxy
+// xyxyxy
+// xyxyxyxy
+// xyxyxyxyxy  <---The effect when you console.log it
+
+function fiveLine(s){
+    let answer = ""
+    for (let i = 1; i <= 5; i++){
+      answer += s.trim().repeat(i) + "\n"
+    }
+    return answer.slice(0,-1)
+  }
+
+//   Coding in function padIt, function accept 2 parameters:
+
+//   str, it's a string representing the string to pad, we need pad some "*" at leftside or rightside of str
+//   n, it's a number, how many times to pad the string.
+
+
+//   Behaviour
+
+// You need to write a loop statement within the function that loops n times. Each time through the loop it will add one * to str, alternating on which side it is padded: the first time will add a * to the left side of str, the second time will add a * to the right side, and so on.
+
+// Finally, return the padded string.
+
+function padIt(str,n){
+    let arr = str.split('')
+    let i = 0
+     while(arr.length < str.length + n){
+       i % 2 == 1 ? arr.push('*') : arr.unshift('*')
+       i++
+   }return arr.join('')
+   }
