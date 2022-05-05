699c651b996ec5a61e6492c8c027d80e8d9bd099
KnuffGeorge <georgethek@gmail.com>
2022-05-04 22:18:27 -0700

    BAC calc

--- codewars0504.js
+++ codewars0504.js
@@ -74,3 +74,23 @@ function padIt(str,n){
        i++
    }return arr.join('')
    }
+
+
+//    Bob drinks too much, and he gets in trouble for it a lot. He drinks so much, in fact, that he has broken the local law enforcement's breathalizer with his alcoholic breath! Bob feels simply aweful, so he wants to make up for it by creating a function that will calculate his blood-alcohol level for them. Unfortunately, Bob has gotten too inebriated to do any programming today, so he needs your help!
+
+//    He did manage to research the formula for blood-alcohol content before getting too drunk, which he describes below.
+   
+//    BAC calculator Formula:
+   
+//    BAC% = (A × 5.14 / W × r) - .015 × H 
+   
+//    A: Total alcohol consumed, in ounces (oz)
+//    W: Body weight, in pounds (lbs)
+//    r: The alcohol distribution ratio, 0.73 for man, and 0.66 for women
+//    H: Time passed since drinking, in hours
+   
+
+   function bloodAlcoholContent(drinks, weight, sex, time){
+    let A = drinks.ounces * drinks.abv
+    return +(A * 5.14 / weight * (sex == 'male' ? 0.73 : 0.66) - (.015 * time)).toFixed(4);
+  }
