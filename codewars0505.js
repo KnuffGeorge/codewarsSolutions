411f45d37d25219320a0eff3688c9aaf9ae77b2f
KnuffGeorge <georgethek@gmail.com>
2022-05-05 21:50:23 -0700

    add to array

--- codewars0505.js
+++ codewars0505.js
@@ -10,3 +10,10 @@
 var ArrowFunc = function(arr) {
     return arr.map( x => String.fromCharCode(x) ).join(''); 
   }
+
+//   Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+
+// The websites array has already been defined for you using the following code:
+
+websites [0] = "codewars";
