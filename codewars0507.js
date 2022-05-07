f76573af518106b3d90475c3de02a3bee7bb586f
KnuffGeorge <georgethek@gmail.com>
2022-05-07 13:48:54 -0700

    Math.round Improved!

--- codewars0507.js
+++ codewars0507.js
@@ -63,3 +63,14 @@ class Labrador extends Dog {
       this.loyal = true;
     }
   }
+//   Your task is to create a method, Math.roundTo(number, precision) which rounds a given number to precision decimal places.
+
+//   You may find the following Math methods useful:
+  
+//       Math.round(x) (of course)
+//       Math.pow(x,y) (returns x to the power of y - e.g. Math.pow(3,4) == 81)
+  
+
+  Math.roundTo = function (number, precision) {
+    return +number.toFixed(precision)
+  }
