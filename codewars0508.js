02efd5fd8d31ea9b7b3f6bb42899c93cee4a533c
KnuffGeorge <georgethek@gmail.com>
2022-05-08 21:29:42 -0700

    no boring zeroes

--- codewars0508.js
+++ codewars0508.js
@@ -25,3 +25,25 @@ function expressionMatter(a, b, c) {
     console.log(a,b,c)
     return Math.max(a * (b + c), a * b * c, a + b * c, (a + b) * c, a + b + c)
   }
+
+//   Numbers ending with zeros are boring.
+
+// They might be fun in your world, but not here.
+
+// Get rid of them. Only the ending ones.
+
+// 1450 -> 145
+// 960000 -> 96
+// 1050 -> 105
+// -1050 -> -105
+
+// Zero alone is fine, don't worry about it. Poor guy anyway
+
+function noBoringZeros(n) {
+    let arr = n.toString().split('')
+    while (arr[arr.length-1] == 0){
+    arr.pop()  
+    }
+      
+    return +arr.join('')
+    }
