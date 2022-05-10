4168cccbbfff650d8b772d1e0442901f0a1b8560
KnuffGeorge <georgethek@gmail.com>
2022-05-09 17:08:39 -0700

    number of days

--- codewars0509.js
+++ codewars0509.js
@@ -17,23 +17,6 @@ function stairsIn20(stairs){
    }
 
 
-   function howManydays(month){
-    let days = 0 
-      switch (month){
-      case 4: case 6: case 9: case 11:
-      days = 30
-      break;
-      case 2:
-      days = 28
-      break;
-      default:
-      days = 31
-      break;
-    
-    }
-    return days;
-  }
-
 //   Complete function howManydays, function accept 1 parameters:month, means the month of year, different month has different days (refer to the following table), return a number that how many days in this month(month is always greater than 0, less than or equal to 12).
 
 // +---------------+-------------+
