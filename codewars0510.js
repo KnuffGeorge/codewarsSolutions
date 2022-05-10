ae78e8570dd07dd5954dd8c41199d60701697966
KnuffGeorge <georgethek@gmail.com>
2022-05-10 12:15:28 -0700

    parity outlier

--- codewars0510.js
+++ codewars0510.js
@@ -47,3 +47,24 @@ function order(words){
 function spinWords(string){
     return string.split(' ').map(x => x.length >= 5 ? x = x.split('').reverse().join('') : x = x).join(' ')
   }
+
+//   Hide Kata Description
+
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+// Examples
+
+// [2, 4, 0, 100, 4, 11, 2602, 36]
+// Should return: 11 (the only odd number)
+
+// [160, 3, 1719, 19, 11, 13, -21]
+// Should return: 160 (the only even number)
+
+function findOutlier(integers){
+    let odd = integers.filter(x => Math.abs(x) % 2 == 1)
+    let even = integers.filter(x => Math.abs(x) % 2 == 0)
+    
+    return even.length > odd.length ? odd[0] : even[0]
+  }
+
+  
+
