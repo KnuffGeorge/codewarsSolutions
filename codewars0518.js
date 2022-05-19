5b4fefc7b09893592ce800bfbf64ecbd9a595b3c
KnuffGeorge <georgethek@gmail.com>
2022-05-18 21:03:37 -0700

    sum of row

--- codewars0518.js
+++ codewars0518.js
@@ -120,3 +120,31 @@ function printerError(s) {
 function incrementer(nums) { 
   return nums.map((el, ind) => (el + ind + 1) % 10)  
  }
+
+
+//  1
+//  3     5
+// 7     9    11
+// 13    15    17    19
+// 21    23    25    27    29
+// ...
+
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+// 1 -->  1
+// 2 --> 3 + 5 = 8
+
+ function rowSumOddNumbers(n) {
+  let row = 1
+  let sum = 0
+  let i = 1
+  while(row <= n){
+  sum = 0
+  for(let j = 1; j <= row; j++){
+  sum +=i 
+  i+=2
+}
+  row++
+}
+  return sum
+  }
