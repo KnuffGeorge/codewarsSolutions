67e091655aa12679093dcc53374b7b85762553b5
KnuffGeorge <georgethek@gmail.com>
2022-05-20 19:08:28 -0700

    consecutive numbers?

--- codewars0520.js
+++ codewars0520.js
@@ -9,3 +9,13 @@ function reverseWords(str) {
             .map(x => x = x.split('').reverse().join(''))
             .join(' ') 
   }
+
+
+//   You are given a list of unique integers arr, and two integers a and b. Your task is to find out whether or not a and b appear consecutively in arr, and return a boolean value (True if a and b are consecutive, False otherwise).
+
+// It is guaranteed that a and b are both present in arr.
+
+function consecutive(arr, a, b) {
+    return arr.map((x, i) => (x == a || x == b) && (arr[i-1] == a || arr[i-1] == b)).includes(true)
+    
+  }
