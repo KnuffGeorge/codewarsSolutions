ad254a1a679460dec3cc9f54a4e6982b82186acf
KnuffGeorge <georgethek@gmail.com>
2022-05-21 15:20:03 -0700

    sum of fizzes and buzzes]

--- codewars0521.js
+++ codewars0521.js
@@ -18,3 +18,23 @@ function removeSmallest(numbers) {
     let minimum = numbers.indexOf(Math.min(...numbers))
     return numbers.filter((x,i) => i !== minimum)
   }
+
+
+//   Your task is to write function findSum.
+
+// Upto and including n, this function will return the sum of all multiples of 3 and 5.
+
+// For example:
+
+// findSum(5) should return 8 (3 + 5)
+
+// findSum(10) should return 33 (3 + 5 + 6 + 9 + 10)
+
+function findSum(n) {
+    let sum = 0
+    for (let i = 1; i <= n; i++){
+      i % 3 === 0 || i % 5 === 0 ? sum += i : sum += 0
+    }
+    
+    return sum;
+  }
