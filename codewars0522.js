aba27fdc79e32b0a8342806f176faefb0960f503
KnuffGeorge <georgethek@gmail.com>
2022-05-22 15:04:16 -0700

    anagram checker

--- codewars0522.js
+++ codewars0522.js
@@ -94,3 +94,19 @@ function nbDig(n, d) {
     }
     return k.join('').split('').filter(x => x == d).length
 }
+
+// An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+// Note: anagrams are case insensitive
+
+// Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+// Examples
+
+//     "foefet" is an anagram of "toffee"
+
+//     "Buckethead" is an anagram of "DeathCubeK"
+
+// write the function isAnagram
+var isAnagram = function(test, original) {
+    return test.toLowerCase().split('').sort().join('') == original.toLowerCase().split('').sort().join('')
+  };
