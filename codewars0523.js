204988e97cf3ad7dc7f022df9886aa5cdf2e1175
KnuffGeorge <georgethek@gmail.com>
2022-05-23 21:56:04 -0700

    checked the exam

--- codewars0523.js
+++ codewars0523.js
@@ -17,3 +17,20 @@ function stray(numbers) {
     }
     }
 
+    // The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
+
+    // The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string (in C the space character is used).
+    
+    // If the score < 0, return 0.
+    
+    // For example:
+    
+    // checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+    // checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+    // checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
+    // checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
+    
+    function checkExam(array1, array2) {
+        let score = array2.map((x,i) => x === array1[i] ? 4 : !x ? 0 :-1).reduce((ac,el) => ac + el, 0)
+        return Math.max(score, 0)
+      }   
