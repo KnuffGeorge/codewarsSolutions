ffb615cf3a1d5e748c5a7563617daa02e9798e49
KnuffGeorge <georgethek@gmail.com>
2022-05-23 22:09:37 -0700

    last one was alphabet position, this one didgit summing forreal

--- codewars0523.js
+++ codewars0523.js
@@ -54,3 +54,21 @@ function stray(numbers) {
         }
         return answer.slice(0,-1)
         }
+
+//         Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+
+// For example: (Input --> Output)
+
+// 10 --> 1
+// 99 --> 18
+// -32 --> 5
+
+// Let's assume that all numbers in the input will be integer values.
+
+function sumDigits(number) {
+    let numbArr = number.toString().split('')
+    if (number < 0){
+      return numbArr.slice(1).reduce((ac, el) => ac + +el, 0)
+    }
+    return numbArr.reduce((ac,el) => ac + +el,0)
+  }
