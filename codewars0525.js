5b87e15c5926fb3eddbcb6ab9d6527929899aa45
KnuffGeorge <georgethek@gmail.com>
2022-05-25 23:07:35 -0700

    minimize array

--- codewars0525.js
+++ codewars0525.js
@@ -37,3 +37,45 @@ function tribonacci(signature,n){
         }
         return answer.slice(0,n)
       }
+
+    //   Task
+
+    //   Given an array of integers , Find the minimum sum which is obtained from summing each Two integers product .
+    //   Notes
+      
+    //       Array/list will contain positives only .
+    //       Array/list will always has even size
+      
+    //   Input >> Output Examples
+      
+    //   minSum({5,4,2,3}) ==> return (22) 
+      
+    //   Explanation:
+      
+    //       The minimum sum obtained from summing each two integers product , 5*2 + 3*4 = 22
+      
+    //   minSum({12,6,10,26,3,24}) ==> return (342)
+      
+    //   Explanation:
+      
+    //       The minimum sum obtained from summing each two integers product , 26*3 + 24*6 + 12*10 = 342
+      
+    //   minSum({9,2,8,7,5,4,0,6}) ==> return (74)
+      
+    //   Explanation:
+      
+    //       The minimum sum obtained from summing each two integers product , 9*0 + 8*2 +7*4 +6*5 = 74
+
+    function minSum(arr) {
+        let min = 0
+        let max = 0
+        let answer = []
+        arr.sort((a,b)=> a - b)
+        while (arr.length > 0){
+          min = arr.pop()
+          max = arr.shift()
+          answer.push(min * max)
+        }
+        return answer.reduce((ac,el) => ac + el, 0)
+      }
+      
