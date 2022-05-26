8a2b1703581babd3384bb873c4989f84a3718b79
KnuffGeorge <georgethek@gmail.com>
2022-05-25 23:10:45 -0700

    convert case

--- codewars0525.js
+++ codewars0525.js
@@ -99,3 +99,21 @@ function tribonacci(signature,n){
         return (bound - (bound % divisor))
       }
       
+
+//       In this Kata, you will be given a string that may have mixed uppercase and lowercase letters and your task is to convert that string to either lowercase only or uppercase only based on:
+
+//       make as few changes as possible.
+//       if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.
+  
+//   For example:
+  
+//   solve("coDe") = "code". Lowercase characters > uppercase. Change only the "D" to lowercase.
+//   solve("CODe") = "CODE". Uppercase characters > lowecase. Change only the "e" to uppercase.
+//   solve("coDE") = "code". Upper == lowercase. Change all to lowercase.
+
+function solve(s){ 
+    return s.split('').filter(x => x === x.toUpperCase()).length > s.split('').filter(x => x === x.toLowerCase()).length ? s.toUpperCase() : s.toLowerCase()
+  }
+
+
+  
