3a26d68419b8fb42b2b143aeb283c9d082cbf4b6
KnuffGeorge <georgethek@gmail.com>
2022-05-25 23:08:29 -0700

    max multiple

--- codewars0525.js
+++ codewars0525.js
@@ -78,4 +78,24 @@ function tribonacci(signature,n){
         }
         return answer.reduce((ac,el) => ac + el, 0)
       }
+
+    //   Task
+
+    //   Given a Divisor and a Bound , Find the largest integer N , Such That ,
+    //   Conditions :
+      
+    //       N is divisible by divisor
+      
+    //       N is less than or equal to bound
+      
+    //       N is greater than 0.
+      
+    //   Notes
+      
+    //       The parameters (divisor, bound) passed to the function are only positive values .
+    //       It's guaranteed that a divisor is Found .
+
+    function maxMultiple(divisor, bound){
+        return (bound - (bound % divisor))
+      }
       
