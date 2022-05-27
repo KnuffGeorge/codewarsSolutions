8c2ae7e9176742676e4cb4d92f98912f89ba96a6
KnuffGeorge <georgethek@gmail.com>
2022-05-27 09:41:39 -0700

    alphabet war

---
+++ codewars0527.js
@@ -0,0 +1,46 @@
+
+// Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
+
+// The left side letters and their power:
+
+//  w - 4
+//  p - 3
+//  b - 2
+//  s - 1
+
+// The right side letters and their power:
+
+//  m - 4
+//  q - 3
+//  d - 2
+//  z - 1
+
+// The other letters don't have power and are only victims.
+
+
+function alphabetWar(fight){
+    let left = {
+     'w': 4,   
+     'p': 3,
+     'b': 2,
+     's': 1,
+    } 
+    let right = {
+     'm': 4,   
+     'q': 3,
+     'd': 2,
+     'z': 1,
+    } 
+    let rightPower = 0
+    let leftPower = 0
+    for (let i = 0; i < fight.length; i++){
+      if (fight[i] in left){
+        leftPower += left[fight[i]]  
+      }if (fight[i] in right){
+        rightPower += right[fight[i]]  
+      }  
+    }  
+    return leftPower === rightPower ? "Let's fight again!" : leftPower < rightPower ? "Right side wins!" : "Left side wins!"
+    }
+
+    
