e40b2f1ae17bfd78b2b048708567d76dd4dec7c6
KnuffGeorge <georgethek@gmail.com>
2022-05-27 13:49:30 -0700

    sum of top 2 numbers in array

--- codewars0527.js
+++ codewars0527.js
@@ -77,3 +77,16 @@ function alphabetWar(fight){
 function wordsToMarks(string){
     return [...string].reduce((ac,el)=> ac + el.charCodeAt(0) - 96, 0)
   }
+
+//   Given a sequence of numbers, find the largest pair sum in the sequence.
+
+// For example
+
+// [10, 14, 2, 23, 19] -->  42 (= 23 + 19)
+// [99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
+
+// Input sequence contains minimum two elements and every element is an integer.
+
+function largestPairSum (numbers) {
+    return numbers.sort((a,b)=> b-a).reduce((ac,el,ind)=> ind < 2 ? ac + el : ac,0)
+  }
