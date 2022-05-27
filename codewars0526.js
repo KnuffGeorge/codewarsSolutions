0ca67750dafb687ef8e68e3b13861acf23d9ce4d
KnuffGeorge <georgethek@gmail.com>
2022-05-26 22:42:38 -0700

    row weights

--- codewars0526.js
+++ codewars0526.js
@@ -12,3 +12,21 @@ function reverseNumber(n) {
     n < 0 ? answer += '-' : null
      return Number(answer + Math.abs(n).toString().split('').reverse().join(''))
   }
+
+//   Scenario
+
+// Several people are standing in a row divided into two teams.
+// The first person goes into team 1, the second goes into team 2, the third goes into team 1, and so on.
+// Task
+
+// Given an array of positive integers (the weights of the people), return a new array/tuple of two integers, where the first one is the total weight of team 1, and the second one is the total weight of team 2.
+// Notes
+
+//     Array size is at least 1.
+//     All numbers will be positive.
+
+function rowWeights(array){
+    let teamOne = array.reduce((ac,el,ind) => ind % 2 === 0 ? ac + el : ac,0)
+    let teamTwo = array.reduce((ac,el,ind) => ind % 2 === 1 ? ac + el : ac,0)
+    return [teamOne, teamTwo]
+  }
