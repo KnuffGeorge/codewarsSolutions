dcbbfc54e58ead9e188b972fbef00b905559684e
KnuffGeorge <georgethek@gmail.com>
2022-05-29 15:46:01 -0700

    last n even numbers

--- codewars0529.js
+++ codewars0529.js
@@ -29,3 +29,15 @@ function fizzbuzz(n)
   answer.push(i % 3 === 0 && i % 5 === 0 ? 'FizzBuzz' : i % 3 === 0 ? 'Fizz' : i % 5 === 0 ? "Buzz" : i)  
   }return answer
 }
+
+// Given an array of numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
+
+// For example:
+
+// ([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) => [4, 6, 8]
+// ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
+// ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
+
+function evenNumbers(array, number) {
+    return array.filter(x=> x % 2 === 0).slice(-number)
+  }
