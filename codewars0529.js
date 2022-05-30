85bfe743b03428118c504595f6bc819db49b5800
KnuffGeorge <georgethek@gmail.com>
2022-05-29 20:26:06 -0700

    greet by name

--- codewars0529.js
+++ codewars0529.js
@@ -41,3 +41,15 @@ function fizzbuzz(n)
 function evenNumbers(array, number) {
     return array.filter(x=> x % 2 === 0).slice(-number)
   }
+
+//   Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+// Example:
+
+// "riley" --> "Hello Riley!"
+// "JACK"  --> "Hello Jack!"
+
+var greet = function(name) {
+    return `Hello ${name.slice(0,1).toUpperCase() + name.slice(1).toLowerCase()}!`
+    };
+
