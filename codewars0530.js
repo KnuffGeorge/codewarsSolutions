e81b84011ee57ac8dee6e2f21718512fd696f76a
KnuffGeorge <georgethek@gmail.com>
2022-05-30 09:50:33 -0700

    power of two?

--- codewars0530.js
+++ codewars0530.js
@@ -11,3 +11,22 @@
 function solve(arr){  
     return arr.map(x=> [...x].reduce((ac,el,ind)=> x.toLowerCase().charCodeAt(ind) - 96 === ind + 1 ? ac += 1 : ac += 0, 0))
     };
+
+//     Complete the function power_of_two/powerOfTwo (or equivalent, depending on your language) that determines if a given non-negative integer is a power of two. From the corresponding Wikipedia entry:
+
+//     a power of two is a number of the form 2n where n is an integer, i.e. the result of exponentiation with number two as the base and integer n as the exponent.
+
+// You may assume the input is always valid.
+// Examples
+
+// isPowerOfTwo(1024) // -> true
+// isPowerOfTwo(4096) // -> true
+// isPowerOfTwo(333)  // -> false
+
+function isPowerOfTwo(n){
+    for (let i = 0; i < 30; i++){
+      if (2 ** i === n){
+        return true
+      }
+    }return false
+  }
