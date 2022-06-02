ee493b20629787336f0a059d5f8266f93154a162
KnuffGeorge <georgethek@gmail.com>
2022-06-01 21:28:47 -0700

    has unique characters

--- codewars0601.js
+++ codewars0601.js
@@ -54,3 +54,12 @@ function gps(s, x) {
 function noOdds( values ){
   return values.filter(x=> x % 2 === 0)
 }
+
+// Write a program to determine if a string contains only unique characters. Return true if it does and false otherwise.
+
+// The string may contain any of the 128 ASCII characters. Characters are case-sensitive, e.g. 'a' and 'A' are considered different characters.
+
+function hasUniqueChars(str){
+  let arr = Array.from(new Set (str.split('')))
+  return arr.length === str.length
+}
