f073d4632fbcc6aab5ee9b829b5168d2cc4154a4
KnuffGeorge <georgethek@gmail.com>
2022-06-06 20:05:52 -0700

    sum even numbers

--- codewars0606.js
+++ codewars0606.js
@@ -36,17 +36,30 @@ function toNumberArray(stringarray){
 
 //   The number of days that it will take for the plant to reach/pass desiredHeight (including the last day in the total count).
 
- 
-  function growingPlant(upSpeed, downSpeed, desiredHeight) {
-    let plantSize = 0
-    let days = 0
-    while (plantSize <= desiredHeight){
-      days++
-      plantSize += upSpeed
-      if(plantSize >= desiredHeight){
-        return days
-      }
-      plantSize -= downSpeed
-    }
-    return days  
+ function growingPlant(upSpeed, downSpeed, desiredHeight) {
+   let plantSize = 0
+   let days = 0
+   while (plantSize <= desiredHeight){
+     days++
+     plantSize += upSpeed
+     if(plantSize >= desiredHeight){
+       return days
+     }
+     plantSize -= downSpeed
+   }
+   return days  
+ }
+
+// Complete the function that takes a sequence of numbers as single parameter. Your function must return the sum of the even values of this sequence.
+
+// Only numbers without decimals like 4 or 4.0 can be even.
+
+// The input is a sequence of numbers: integers and/or floats.
+// Examples
+
+// [4, 3, 1, 2, 5, 10, 6, 7, 9, 8]  -->  30   # because 4 + 2 + 10 + 6 + 8 = 30
+// []                               -->  0
+
+function sumEvenNumbers(input) {
+    return input.reduce((ac,el) => el % 2 === 0 ? ac + el : ac, 0)
   }
