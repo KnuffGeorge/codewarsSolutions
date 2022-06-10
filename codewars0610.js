61ef40f859a40e273dc32642386e28f51c80d78f
KnuffGeorge <georgethek@gmail.com>
2022-06-10 16:26:33 -0700

    tidy number

--- codewars0610.js
+++ codewars0610.js
@@ -6,3 +6,15 @@
 function disariumNumber(n){
     return n == [...n.toString()].reduce((ac,el,ind)=> ac + el ** (ind+1), 0) ? "Disarium !!" : "Not !!"
   }
+
+//   Definition
+
+// A Tidy number is a number whose digits are in non-decreasing order.
+// Task
+
+// Given a number, Find if it is Tidy or not . 
+
+function tidyNumber(n){
+    let numbArr = [...n.toString()]
+    return numbArr.filter((x,i)=> x <= numbArr[i+1] || i === numbArr.length - 1).length === numbArr.length
+  }
