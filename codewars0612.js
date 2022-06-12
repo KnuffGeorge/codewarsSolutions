9eec7f5642e9efe60bde97e04d4add7e0209f8e1
KnuffGeorge <georgethek@gmail.com>
2022-06-12 16:34:03 -0700

    nth smallest element

---
+++ codewars0612.js
@@ -0,0 +1,7 @@
+// Task
+
+// Given an array/list [] of integers , Find the Nth smallest element in this array of integers
+
+function nthSmallest(arr, pos){
+    return arr.sort((a,b)=>a-b)[pos-1]
+  }
