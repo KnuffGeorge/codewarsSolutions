80f451f5a0700ab2f6688ec13e8067fe4e0880c6
KnuffGeorge <georgethek@gmail.com>
2022-06-12 16:35:04 -0700

    simple sort

--- codewars0612.js
+++ codewars0612.js
@@ -5,3 +5,11 @@
 function nthSmallest(arr, pos){
     return arr.sort((a,b)=>a-b)[pos-1]
   }
+
+//   Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+// input: names - unsorted array
+// output: sorted array
+sortme = function( names ){
+    return names.sort()
+  }
