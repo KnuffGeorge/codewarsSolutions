056c1cdd363239d22a2814efaae591d327354058
KnuffGeorge <georgethek@gmail.com>
2022-06-13 16:38:55 -0700

    first n multiples of n

--- codewars0613.js
+++ codewars0613.js
@@ -13,3 +13,20 @@ function jumpingNumber(n){
       }
     }return "Jumping!!"
   }
+
+//   Implement a function, multiples(m, n), which returns an array of the first m multiples of the real number n. Assume that m is a positive integer.
+
+// Ex.
+
+// multiples(3, 5.0)
+
+// should return
+
+// [5.0, 10.0, 15.0]
+
+function multiples(m, n){
+    let answer = []
+    for(let i = 1; i <= m; i++){
+      answer.push(i * n)
+    }return answer
+  }
