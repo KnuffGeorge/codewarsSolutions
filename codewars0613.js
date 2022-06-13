426cb6ec863c9666e2e8241b9a39e656b28d87e1
KnuffGeorge <georgethek@gmail.com>
2022-06-13 16:40:12 -0700

    filter number out of string

--- codewars0613.js
+++ codewars0613.js
@@ -30,3 +30,12 @@ function multiples(m, n){
       answer.push(i * n)
     }return answer
   }
+
+//   Oh, no! The number has been mixed up with the text. Your goal is to retrieve the number from the text, can you return the number back to its original state?
+//   Task
+  
+//   Your task is to return a number from a string.  
+
+var FilterString = function(value) {
+    return +[...value].filter(x=> !isNaN(+x)).join('')
+  }
