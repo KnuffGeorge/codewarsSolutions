723f4195982023b7f555660fdef0f4f3692df1f6
KnuffGeorge <georgethek@gmail.com>
2022-06-17 18:01:05 -0700

    split in parts

--- codewars0617.js
+++ codewars0617.js
@@ -48,3 +48,21 @@ function houseNumbersSum(inputArray) {
         }
     
 
+        // The aim of this kata is to split a given string into different strings of equal size (note size of strings is passed to the method)
+
+        // Example:
+        
+        // Split the below string into other strings of size #3
+        
+        // 'supercalifragilisticexpialidocious'
+        
+        // Will return a new string
+        // 'sup erc ali fra gil ist ice xpi ali doc iou s'
+        
+        var splitInParts = function(s, partLength){
+            let answer = ''
+            for (let i = 0; i<s.length; i += partLength){
+              answer += s.slice(i,i+partLength) + " "
+            }
+            return answer.slice(0,-1)
+          }
