b10e5f2bd3da61b5838df446937159456ec51d43
KnuffGeorge <georgethek@gmail.com>
2022-06-20 18:16:44 -0700

    fill my hard drive

--- codewars0620.js
+++ codewars0620.js
@@ -12,3 +12,23 @@ function sortMyString(S) {
     }
     return `${even} ${odd}`
   }
+
+//   Your task is to determine how many files of the copy queue you will be able to save into your Hard Disk Drive. The files must be saved in the order they appear in the queue.
+//   Input:
+  
+//       Array of file sizes (0 <= s <= 100)
+//       Capacity of the HD (0 <= c <= 500)
+  
+//   Output:
+  
+//       Number of files that can be fully saved in the HD.
+  
+function save(sizes, hd) {
+    let sum=0
+    for (let i = 0; i < sizes.length; i++) {
+      sum += sizes[i];
+      
+      if (sum>hd)  return i        
+    }
+    return sizes.length 
+    }
