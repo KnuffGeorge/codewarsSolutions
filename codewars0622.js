c5a0e92d6ddff1c040baa54c191c577ba7cc2fb5
KnuffGeorge <georgethek@gmail.com>
2022-06-22 17:27:01 -0700

    double sort

--- codewars0622.js
+++ codewars0622.js
@@ -22,3 +22,15 @@ function duplicates(array){
   set.forEach(x=> pairs += Math.floor(array.filter(y=> y===x).length / 2))
     return pairs
   }
+
+//   Simple enough this one - you will be given an array. The values in the array will either be numbers or strings, or a mix of both. You will not get an empty array, nor a sparse one.
+
+// Your job is to return a single array that has first the numbers sorted in ascending order, followed by the strings sorted in alphabetic order. The values must maintain their original type.
+
+// Note that numbers written as strings are strings and must be sorted with the other strings.
+
+function dbSort(a){
+    let numbers = a.filter(x=> x !== x.toString()).sort((a,b)=>a-b)
+    let letters = a.filter(x=> x === x.toString()).sort()
+    return numbers.concat(letters)
+    }
