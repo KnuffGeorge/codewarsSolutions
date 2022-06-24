fa1f937b764c79001b951495ed7b203240a55e13
KnuffGeorge <georgethek@gmail.com>
2022-06-23 17:32:31 -0700

    insert dashes between odd numbers

--- codewars0623.js
+++ codewars0623.js
@@ -59,3 +59,10 @@ function goodVsEvil(good, evil){
     let evilScore = evil.split(' ').reduce((ac,el,ind)=> ac + (+el * evilPts[ind]),0)
     return goodScore > evilScore ? 'Battle Result: Good triumphs over Evil' : goodScore === evilScore ? 'Battle Result: No victor on this battle field' : 'Battle Result: Evil eradicates all trace of Good'
     }
+
+    // Write a function insert_dash(num) / insertDash(num) / InsertDash(int num) that will insert dashes ('-') between each two odd digits in num. For example: if num is 454793 the output should be 4547-9-3. Don't count zero as an odd digit.
+
+    // Note that the number will always be non-negative (>= 0).
+    function insertDash(num) {
+        return num.toString().split('').map((x,i,arr)=> +x % 2 === 1 && +arr[i+1] % 2===1 ? x + "-" : x).join('')
+     }
