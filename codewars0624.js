b7d01009a9ea0f1fc6e3664f6464986e697d1fea
KnuffGeorge <georgethek@gmail.com>
2022-06-24 17:45:28 -0700

    encode/decode the vowels

--- codewars0624.js
+++ codewars0624.js
@@ -70,4 +70,31 @@ decodeMorse = function(morseCode){
     return morseCode.trim().split('   ').map(x => x.split(' ').map(y => MORSE_CODE[y]).join('')).join(' ');
  }
 
+//  Step 1: Create a function called encode() to replace all the lowercase vowels in a given string with numbers according to the following pattern:
+
+// a -> 1
+// e -> 2
+// i -> 3
+// o -> 4
+// u -> 5
+
+// For example, encode("hello") would return "h2ll4". There is no need to worry about uppercase vowels in this kata.
+
+// Step 2: Now create a function called decode() to turn the numbers back into vowels according to the same pattern shown above.
+
+// For example, decode("h3 th2r2") would return "hi there".
+
+// For the sake of simplicity, you can assume that any numbers passed into the function will correspond to vowels.
+
+let vowels = "aeiou"
+let encKey = {a:1, e:2, i:3, o:4, u:5}
+let decKey = {1:"a", 2:"e", 3:"i", 4:"o", 5:"u"}
+function encode(string) {
+  return [...string].map(x=> vowels.includes(x) ? encKey[x] : x).join('')
+}
+
+
+function decode(string) {
+return [...string].map(x=> !isNaN(+x) ? decKey[x] : x).join('')
+}
 
