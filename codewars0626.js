b88445bb2e9ed96660cf3ceb74f487af9d6e1f8c
KnuffGeorge <georgethek@gmail.com>
2022-06-26 22:04:06 -0700

    most common number in array

--- codewars0626.js
+++ codewars0626.js
@@ -127,5 +127,18 @@ multiplicationTable = function(size) {
       }answer.push(arr)
     }return answer
   }
+
+//   Complete the method which returns the number which is most frequent in the given input array. If there is a tie for most frequent number, return the largest number among them.
+
+//   Note: no empty arrays will be given.
+//   Examples
   
+//   [12, 10, 8, 12, 7, 6, 4, 10, 12]              -->  12
+//   [12, 10, 8, 12, 7, 6, 4, 10, 12, 10]          -->  12
+//   [12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]  -->   3
+  
+function highestRank(arr){
+    let set = Array.from(new Set(arr))
+    return set.sort((a,b)=> arr.filter(x=>x===b).length - arr.filter(x=>x===a).length)[0]
+    }
   
