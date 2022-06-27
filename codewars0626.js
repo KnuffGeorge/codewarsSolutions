6d4d1dac26aca44a7803892dc533c37c8a9ad38d
KnuffGeorge <georgethek@gmail.com>
2022-06-26 21:55:37 -0700

    make the dead fish swim

--- codewars0626.js
+++ codewars0626.js
@@ -50,4 +50,25 @@ function solution(string) {
     return string.split('').map((x => x===x.toUpperCase()? " "+x: x)).join('');
 }
 
+// Write a simple parser that will parse and run Deadfish.
+
+// Deadfish has 4 commands, each 1 character long:
+
+//     i increments the value (initially 0)
+//     d decrements the value
+//     s squares the value
+//     o outputs the value into the return array
+
+// Invalid characters should be ignored.
+
+// parse("iiisdoso") => [ 8, 64 ]
+
+function parse(data)
+{let value = 0
+let answer = []
+data.split('').forEach(x=> x == "i" ? value++ : x == "d" ? value-- : x == "s" ? value *= value : x == "o" ? answer.push(value) : null)
+return answer
+}
+
+
   
