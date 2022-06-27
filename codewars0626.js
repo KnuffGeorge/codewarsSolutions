5ebc0bc7568ec692314c4d4c04b7449475b4e12e
KnuffGeorge <georgethek@gmail.com>
2022-06-26 22:07:03 -0700

    last survivor

--- codewars0626.js
+++ codewars0626.js
@@ -150,3 +150,32 @@ function highestRank(arr){
         return str.includes('coverage') ? str.split('coverage').join('covfefe') :
         str + ' covfefe';
       }
+
+    //   You are given a string of letters and an array of numbers.
+    //   The numbers indicate positions of letters that must be removed, in order, starting from the beginning of the array.
+    //   After each removal the size of the string decreases (there is no empty space).
+    //   Return the only letter left.
+      
+    //   Example:
+      
+    //   let str = "zbk", arr = [0, 1]
+    //       str = "bk", arr = [1]
+    //       str = "b", arr = []
+    //       return 'b'
+      
+    //   Notes
+      
+    //       The given string will never be empty.
+    //       The length of the array is always one less than the length of the string.
+    //       All numbers are valid.
+    //       There can be duplicate letters and numbers.
+
+    function lastSurvivor(letters, coords) {
+        let index = 0
+        let letArr = letters.split('')
+          for (let i = 1; i<letters.length; i++){
+            index = coords.shift()
+            letArr.splice(index,1)   
+        }return letArr.join('')
+        }   
+      
