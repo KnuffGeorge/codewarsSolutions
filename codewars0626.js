f535df71fb021683abee2aec3a0d202e35d27234
KnuffGeorge <georgethek@gmail.com>
2022-06-26 22:09:37 -0700

    even vs. odd

--- codewars0626.js
+++ codewars0626.js
@@ -223,3 +223,17 @@ function highestRank(arr){
             console.log(arr)
             return arr.slice(1)
           }
+
+        //   Given a string of digits confirm whether the sum of all the individual even digits are greater than the sum of all the indiviudal odd digits. Always a string of numbers will be given.
+
+        //   If the sum of even numbers is greater than the odd numbers return: "Even is greater than Odd"
+      
+        //   If the sum of odd numbers is greater than the sum of even numbers return: "Odd is greater than Even"
+      
+        //   If the total of both even and odd numbers are identical return: "Even and Odd are the same"
+      
+        function evenOrOdd(str) {
+            let even = [...str].reduce((ac,el)=> el % 2 === 0 ? ac + +el : ac, 0)
+            let odd = [...str].reduce((ac,el)=> el % 2 === 1 ? ac + +el : ac, 0)
+            return even > odd ? 'Even is greater than Odd' : even < odd ? 'Odd is greater than Even' : 'Even and Odd are the same'
+              }
