4c00d941cf9913f918ce3e594fc1365ddeea5982
KnuffGeorge <georgethek@gmail.com>
2022-06-26 22:00:44 -0700

    evens * last

--- codewars0626.js
+++ codewars0626.js
@@ -98,4 +98,16 @@ function solve(a,b){
     return b.map(x => a.filter(y => y == x).length);
   }
 
+//   Given a sequence of integers, return the sum of all the integers that have an even index (odd index in COBOL), multiplied by the integer at the last index.
+
+// Indices in sequence start from 0.
+
+// If the sequence is empty, you should return 0.
+
+function evenLast(numbers) {
+    if (numbers.length === 0) return 0
+    return (numbers.reduce((ac,el,ind)=> ind % 2 == 0 ? ac + el : ac,0) * numbers[numbers.length-1])
+  }
+
+
   
