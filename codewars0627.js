b7f9f2c67b854e9ec2b08e39628bcdafc2f2f8e7
KnuffGeorge <georgethek@gmail.com>
2022-06-27 22:13:44 -0700

    make an acronym string

--- codewars0627.js
+++ codewars0627.js
@@ -13,3 +13,15 @@ function uniqueSum(lst){
 function fireFight(s){
     return s.split("Fire").join("~~")
   }
+
+//   In this exercise, a string is passed to a method and a new string has to be returned with the first character of each word in the string.
+
+// For example:
+
+// "This Is A Test" ==> "TIAT"
+
+// Strings will only contain letters and spaces, with exactly 1 space between words, and no leading/trailing spaces.
+
+function makeString(s){
+    return s.split(' ').map( x => x.charAt(0) ).join('');
+  }
