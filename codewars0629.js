bd86c6ee95a8c454bb95776e18bedf42ade1ea8c
KnuffGeorge <georgethek@gmail.com>
2022-06-29 22:51:07 -0700

    pyramid array

--- codewars0629.js
+++ codewars0629.js
@@ -129,4 +129,26 @@
         
         return answer;
       }
+
+    //   Write a function that when given a number >= 0, returns an Array of ascending length subarrays.
+
+    //   pyramid(0) => [ ]
+    //   pyramid(1) => [ [1] ]
+    //   pyramid(2) => [ [1], [1, 1] ]
+    //   pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
+      
+    //   Note: the subarrays should be filled with 1s
+
+    function pyramid(n) {
+        let answer = []
+        let arr = []
+        for (let i = 1; i <= n; i++){
+          arr = []
+          for(let j = 0; j<i;j++){
+          arr.push(1)
+      }  
+          answer.push(arr)
+        }return answer
+      }
+      
       
