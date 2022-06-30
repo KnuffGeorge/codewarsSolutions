7cedbc21621e29cdce72df7ee9a9c94b6a11573b
KnuffGeorge <georgethek@gmail.com>
2022-06-29 22:43:55 -0700

    dict from 2 arrays

--- codewars0629.js
+++ codewars0629.js
@@ -36,3 +36,17 @@
           } 
         }return order
       }
+
+    //   There are two lists, possibly of different lengths. The first one consists of keys, the second one consists of values. Write a function createDict(keys, values) that returns a dictionary created from keys and values. If there are not enough values, the rest of keys should have a None (JS null)value. If there not enough keys, just ignore the rest of values.
+
+    function createDict(keys, values){
+        let obj = {};
+        for (let i  = 0; i < keys.length; i++){
+          if (values[i] != undefined){
+            obj[keys[i]] = values[i];
+          }else{
+            obj[keys[i]] = null;
+          }
+        }
+        return obj;
+      }   
