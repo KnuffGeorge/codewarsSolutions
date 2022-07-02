c60714a3e12aa956f08a41c926b4ca1a639a2686
KnuffGeorge <georgethek@gmail.com>
2022-07-01 22:58:45 -0700

    broken photocopier

--- codewars0701.js
+++ codewars0701.js
@@ -50,5 +50,13 @@ function lastDigit(n, d) {
     let arr = n.toString().split('').map(x=>+x)
     return arr.length > d ? arr.slice(arr.length - d) : arr
   }
+
+//   The bloody photocopier is broken... Just as you were sneaking around the office to print off your favourite binary code!
+
+// Instead of copying the original, it reverses it: '1' becomes '0' and vice versa.
+
+// Given a string of binary, return the version the photocopier gives you as a string.
   
-  
+function broken(x){
+    return x.split('').map((x) => x == '1' ? '0' : '1').join('');
+  }
