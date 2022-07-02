f605f5d95c9249dc196b5d91199791be66a72026
KnuffGeorge <georgethek@gmail.com>
2022-07-01 22:57:09 -0700

    last digits of a number

--- codewars0701.js
+++ codewars0701.js
@@ -39,4 +39,16 @@ function unusedDigits(nums) {
       !str.includes(i) ? answer += i : null
     }return answer
   }
+
+//   Your job is to implement a function which returns the last D digits of an integer N as a list.
+//   Special cases:
+  
+//       If D > (the number of digits of N), return all the digits.
+//       If D <= 0, return an empty list.
+
+function lastDigit(n, d) {
+    let arr = n.toString().split('').map(x=>+x)
+    return arr.length > d ? arr.slice(arr.length - d) : arr
+  }
+  
   
