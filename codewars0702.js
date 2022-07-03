739699c692471fc78b18d0a3094219d80ccc3b33
KnuffGeorge <georgethek@gmail.com>
2022-07-02 23:03:40 -0700

    gordon ramsay speak

--- codewars0702.js
+++ codewars0702.js
@@ -24,3 +24,16 @@ function getMissingElement(superImportantArray){
       if (!superImportantArray.includes(i)) return i;
     }
   }
+
+//   Gordon Ramsay shouts. He shouts and swears. There may be something wrong with him.
+
+//   Anyway, you will be given a string of four words. Your job is to turn them in to Gordon language.
+  
+//   Rules:
+  
+//   Obviously the words should be Caps, Every word should end with '!!!!', Any letter 'a' or 'A' should become '@', Any other vowel should become '*'.
+
+function gordon(a){
+    let vowels = "AEIOU"
+    return a.split(' ').map(x=> x.toUpperCase() + '!!!!').join(' ').split('').map(x=> x== "A" ? "@" : vowels.includes(x) ? "*" : x).join("")
+  }
