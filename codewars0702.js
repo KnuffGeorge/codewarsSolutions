a9c88ba4430c754f91ffeda566f3dc8cc00032c3
KnuffGeorge <georgethek@gmail.com>
2022-07-02 23:06:02 -0700

    nicknameGenerator

--- codewars0702.js
+++ codewars0702.js
@@ -54,4 +54,27 @@ function stantonMeasure(arr){
     let stanton = arr.filter(x=> x == 1).length
     return arr.filter(x=> x == stanton).length
   }
-  
+
+//   Nickname Generator
+
+// Write a function, nicknameGenerator that takes a string name as an argument and returns the first 3 or 4 letters as a nickname.
+
+// If the 3rd letter is a consonant, return the first 3 letters.
+
+// nickname("Robert") //=> "Rob"
+// nickname("Kimberly") //=> "Kim"
+// nickname("Samantha") //=> "Sam"
+
+// If the 3rd letter is a vowel, return the first 4 letters.
+
+// nickname("Jeannie") //=> "Jean"
+// nickname("Douglas") //=> "Doug"
+// nickname("Gregory") //=> "Greg"
+
+// If the string is less than 4 characters, return "Error: Name too short".
+
+function nicknameGenerator(name){
+    let vowels = "aeiou";
+    if (name.length < 4) return "Error: Name too short"
+    return (vowels.includes(name[2])) ? name.slice(0, 4) : name.slice(0, 3)
+  }
