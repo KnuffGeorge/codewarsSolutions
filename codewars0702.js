2d18118054cc36a6c9aa0a7c4c94004fd4f82c58
KnuffGeorge <georgethek@gmail.com>
2022-07-02 23:02:54 -0700

    return the missing element

--- codewars0702.js
+++ codewars0702.js
@@ -11,4 +11,16 @@ function battle(x, y) {
     let xScore = [...x].reduce((ac,el)=> el.charCodeAt() - 64 + ac, 0)
     let yScore = [...y].reduce((ac,el)=> el.charCodeAt() - 64 + ac, 0)
     return xScore > yScore ? x : xScore == yScore ? "Tie!" : y 
+}
+
+// Fellow code warrior, we need your help! We seem to have lost one of our sequence elements, and we need your help to retrieve it!
+
+// Our sequence given was supposed to contain all of the integers from 0 to 9 (in no particular order), but one of them seems to be missing.
+
+// Write a function that accepts a sequence of unique integers between 0 and 9 (inclusive), and returns the missing element.
+
+function getMissingElement(superImportantArray){
+    for (let i = 0; i < 10; i++) {
+      if (!superImportantArray.includes(i)) return i;
+    }
   }
