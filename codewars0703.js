aedc8dd44c0d0c8ffd7789c08783c844b3fcd51a
KnuffGeorge <georgethek@gmail.com>
2022-07-03 23:13:01 -0700

    is 1st argument divisible by all others?

--- codewars0703.js
+++ codewars0703.js
@@ -42,3 +42,17 @@ function mean(lst){
 function unscrambleEggs(word){
     return word.split("egg").join("")
   }
+
+//   Create a function isDivisible(n,...) that checks if the first argument n is divisible by all other arguments (return true if no other arguments)
+
+//   Example:
+  
+//   isDivisible(6,1,3)--> true because 6 is divisible by 1 and 3
+//   isDivisible(12,2)--> true because 12 is divisible by 2
+//   isDivisible(100,5,4,10,25,20)--> true
+//   isDivisible(12,7)--> false because 12 is not divisible by 7
+
+function isDivisible(){
+    return ![...arguments].map((x,i,arr)=> arr[0] % x == 0).includes(false)
+  }
+  
