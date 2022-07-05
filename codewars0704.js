b63c0f0dc30cc70c1610f1d2f01243699903c906
KnuffGeorge <georgethek@gmail.com>
2022-07-04 23:24:42 -0700

    drone flyby

--- codewars0704.js
+++ codewars0704.js
@@ -87,4 +87,19 @@ function moveTen(s){
     let codes = s.split('').map(x=> x.charCodeAt() + 10)
     return codes.map(x=> x > 122 ? String.fromCharCode(x-26) : String.fromCharCode(x)).join('')
     }
+
+    // The other day I saw an amazing video where a guy hacked some wifi controlled lightbulbs by flying a drone past them. Brilliant.
+
+    // In this kata we will recreate that stunt... sort of.
+    
+    // You will be given two strings: lamps and drone. lamps represents a row of lamps, currently off, each represented by x. When these lamps are on, they should be represented by o.
+    
+    // The drone string represents the position of the drone T (any better suggestion for character??) and its flight path up until this point =. The drone always flies left to right, and always begins at the start of the row of lamps. Anywhere the drone has flown, including its current position, will result in the lamp at that position switching on.
+    
+    // Return the resulting lamps string. See example tests for more clarity.
+
+    function flyBy(lamps, drone){
+        let position = drone.indexOf("T")
+        return [...lamps].map((x,i)=> i > position ? "x" : "o").join('')
+        }
   
