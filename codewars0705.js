f8344f8771c83d7d03d8de38ee09cc350df47399
KnuffGeorge <georgethek@gmail.com>
2022-07-05 23:28:34 -0700

    multiples into an array

--- codewars0705.js
+++ codewars0705.js
@@ -20,3 +20,24 @@ function noRepeat(str) {
     function smallEnough(a, limit) {
         return a.every(x => x <= limit);
       }  
+
+    //   Print all numbers up to 3rd parameter which are multiple of both 1st and 2nd parameter.
+
+    //   Python, Javascript, Java, Ruby versions: return results in a list/array
+      
+    //   NOTICE:
+      
+    //       Do NOT worry about checking zeros or negative values.
+    //       To find out if 3rd parameter (the upper limit) is inclusive or not, check the tests, it differs in each translation
+
+    // Javascript: return multiples into an array
+function multiples(s1,s2,s3){
+    let arr = []
+    let num = Math.min(s1,s2)
+    while (num < s3){
+      if (num % s1 === 0 && num % s2 === 0 && num < s3) arr.push(num)
+      num += Math.min(s1,s2)
+    }
+    return arr;
+  }
+      
