aae4012656a30623d64a2833638a6ce5bcba89df
KnuffGeorge <georgethek@gmail.com>
2022-07-05 23:30:13 -0700

    whos online?

--- codewars0705.js
+++ codewars0705.js
@@ -55,4 +55,56 @@ function nthChar(words){
     return words.map((x,i) => x[i]).join('')
    }
   
-      
+//    You have a group chat application, but who is online!?
+
+//    You want to show your users which of their friends are online and available to chat!
+   
+//    Given an input of an array of objects containing usernames, status and time since last activity (in mins), create a function to work out who is online, offline and away.
+   
+//    If someone is online but their lastActivity was more than 10 minutes ago they are to be considered away.
+   
+//    The input data has the following structure:
+   
+//    [{
+//      username: 'David',
+//      status: 'online',
+//      lastActivity: 10
+//    }, {
+//      username: 'Lucy', 
+//      status: 'offline',
+//      lastActivity: 22
+//    }, {
+//      username: 'Bob', 
+//      status: 'online',
+//      lastActivity: 104
+//    }]
+   
+//    The corresponding output should look as follows:
+   
+//    {
+//      online: ['David'],
+//      offline: ['Lucy'],
+//      away: ['Bob']
+//    }
+   
+//    If for example, no users are online the output should look as follows:
+   
+//    {
+//      offline: ['Lucy'],
+//      away: ['Bob']
+//    }
+
+const whosOnline = (friends) => {
+    let online = friends.filter(x=> x.status === 'online' && x.lastActivity <= 10).map(x=> x.username)
+    let offline = friends.filter(x=> x.status === 'offline').map(x=> x.username)
+    let away = friends.filter(x=> x.status === 'online' && x.lastActivity > 10).map(x=> x.username)
+    
+    let answer = {}
+    online.length > 0 ? answer.online = online : null
+    offline.length > 0 ? answer.offline = offline : null
+    away.length > 0 ? answer.away = away : null
+     
+    return answer  
+    
+    }
+   
