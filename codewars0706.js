c00c464087efefbdb2fe0e9a75249b1c80fa96a4
KnuffGeorge <georgethek@gmail.com>
2022-07-06 23:16:46 -0700

    think + test

---
+++ codewars0706.js
@@ -0,0 +1,11 @@
+// No Story
+
+// No Description
+
+// Only by Thinking and Testing
+
+// Look at result of testcase, guess the code!
+
+function testit(a,b){
+    return Array.from(new Set(a)).concat(Array.from(new Set(b))).sort((a, b) => a - b);
+  }
