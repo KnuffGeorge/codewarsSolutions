8b3e7ae743f83d5cf94946c7118a7b651e81bb4f
KnuffGeorge <georgethek@gmail.com>
2022-07-06 23:17:21 -0700

    powers of 3

--- codewars0706.js
+++ codewars0706.js
@@ -9,3 +9,21 @@
 function testit(a,b){
     return Array.from(new Set(a)).concat(Array.from(new Set(b))).sort((a, b) => a - b);
   }
+
+//   Given a positive integer N, return the largest integer k such that 3^k < N.
+
+//   For example,
+  
+//   largest_power(3) == 0
+//   largest_power(4) == 1
+  
+//   You may assume that the input to your function is always a positive integer.
+
+function largestPower(n){
+    let k = 0;
+    while (3**k<n) {
+      k++;
+    }
+    return k-1;
+  }
+  
