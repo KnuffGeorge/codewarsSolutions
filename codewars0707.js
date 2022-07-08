832ca20df31edd0182862fb327b8da2403e99b45
KnuffGeorge <georgethek@gmail.com>
2022-07-07 23:33:28 -0700

    new cashier doesn't know about spacebar

--- codewars0707.js
+++ codewars0707.js
@@ -33,3 +33,47 @@ function solve(s,g){
 function solution(string,limit){
     return string.length <= limit ? string : string.slice(0,limit) + '...';
 }
+
+// Some new cashiers started to work at your restaurant.
+
+// They are good at taking orders, but they don't know how to capitalize words, or use a space bar!
+
+// All the orders they create look something like this:
+
+// "milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza"
+
+// The kitchen staff are threatening to quit, because of how difficult it is to read the orders.
+
+// Their preference is to get the orders as a nice clean string with spaces and capitals like so:
+
+// "Burger Fries Chicken Pizza Pizza Pizza Sandwich Milkshake Milkshake Coke"
+
+// The kitchen staff expect the items to be in the same order as they appear in the menu.
+
+// The menu items are fairly simple, there is no overlap in the names of the items:
+
+// 1. Burger
+// 2. Fries
+// 3. Chicken
+// 4. Pizza
+// 5. Sandwich
+// 6. Onionrings
+// 7. Milkshake
+// 8. Coke
+
+// function getOrder(input) {
+//     let order = []
+//     let menu =[
+//     'Burger',
+//     'Fries',
+//     'Chicken',
+//     'Pizza',
+//     'Sandwich',
+//     'Onionrings',
+//     'Milkshake',
+//     'Coke', 
+//     ]
+//     menu.forEach(x=> order.push((x + " ").repeat(input.split(x.toLowerCase()).length - 1)))
+//     return order.join('').trim();
+//   }
+
