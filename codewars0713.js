b21ba22ace21196dc680324c5f6db9139fac1a25
KnuffGeorge <georgethek@gmail.com>
2022-07-13 23:55:58 -0700

    is it a number?

--- codewars0713.js
+++ codewars0713.js
@@ -33,3 +33,23 @@ function diamond(n){
     }
     return diamond
   }
+
+//   Given a string s, write a method (function) that will return true if its a valid single integer or floating number or false if its not.
+
+// Valid examples, should return true:
+
+// isDigit("3")
+// isDigit("  3  ")
+// isDigit("-3.23")
+
+// should return false:
+
+// isDigit("3-4")
+// isDigit("  3   5")
+// isDigit("3 5")
+// isDigit("zero")
+
+function isDigit(s) {
+    let x = s.trim().length > 0 ? (Math.abs(+s) + "") : "false"
+    return   !isNaN(+x) 
+  }
