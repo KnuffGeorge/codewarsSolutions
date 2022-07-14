cb2c5f6f98acee6ee55e109854c01fefba1adfa2
KnuffGeorge <georgethek@gmail.com>
2022-07-13 23:54:59 -0700

    gimme a diamond

--- codewars0713.js
+++ codewars0713.js
@@ -15,3 +15,21 @@ function findUniq(arr) {
       }
     }
   }
+
+//   Jamie is a programmer, and James' girlfriend. She likes diamonds, and wants a diamond string from James. Since James doesn't know how to make this happen, he needs your help.
+//   Task
+  
+//   You need to return a string that looks like a diamond shape when printed on the screen, using asterisk (*) characters. Trailing spaces should be removed, and every line must be terminated with a newline character (\n).
+  
+//   Return null/nil/None/... if the input is an even number or negative, as it is not possible to print a diamond of even or negative size.
+
+function diamond(n){
+    if (n%2==0 || n<1) return null
+    let diamond = ""
+    for (let i = 1; i<=n; i+=2){
+      diamond += (i==n ? "" : " ".repeat ((n-i)/2)) + "*".repeat(i) + "\n"
+    }for (let i = n-2; i>=1; i-=2){
+      diamond += " ".repeat((n-i)/2) + "*".repeat(i) +"\n"
+    }
+    return diamond
+  }
