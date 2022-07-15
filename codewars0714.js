45c9da64c13469e5ce55aa751d73af8233c15c6d
KnuffGeorge <georgethek@gmail.com>
2022-07-14 23:58:06 -0700

    build a tower

--- codewars0714.js
+++ codewars0714.js
@@ -11,3 +11,34 @@
 function arrayDiff(a, b) {
     return a.filter(x => !b.includes(x));
   }
+
+//   Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+
+// For example, a tower with 3 floors looks like this:
+
+// [
+//   "  *  ",
+//   " *** ", 
+//   "*****"
+// ]
+
+// And a tower with 6 floors looks like this:
+
+// [
+//   "     *     ", 
+//   "    ***    ", 
+//   "   *****   ", 
+//   "  *******  ", 
+//   " ********* ", 
+//   "***********"
+// ]
+
+function towerBuilder(nFloors) {
+    let tower = []
+    let bricks = "*" 
+    for (let i = 1; i <= nFloors; i++){
+      let spaces = (nFloors - i)
+      tower.push(" ".repeat(spaces) + bricks + " ".repeat(spaces))
+      bricks += "**"
+    }return tower
+  }
