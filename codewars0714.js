e79916c5879f9beb772bfe06414dc165978ccf53
KnuffGeorge <georgethek@gmail.com>
2022-07-15 00:05:33 -0700

    count the valid smileys

--- codewars0714.js
+++ codewars0714.js
@@ -42,3 +42,33 @@ function towerBuilder(nFloors) {
       bricks += "**"
     }return tower
   }
+
+//   Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+//   Rules for a smiling face:
+  
+//       Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+//       A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+//       Every smiling face must have a smiling mouth that should be marked with either ) or D
+  
+//   No additional characters are allowed except for those mentioned.
+  
+//   Valid smiley face examples: :) :D ;-D :~)
+//   Invalid smiley faces: ;( :> :} :]
+
+//return the total number of smiling faces in the array
+function countSmileys(arr) {
+    let validChars = {
+      0: ":;",
+      1: "~-)D",
+      2: ")D"
+    } 
+    return arr.filter(x=>{
+      for (let i=0; i<x.length;i++){
+        if (!validChars[i].includes(x[i])){
+          return false
+        }
+      }return true
+    }).length
+    
+    }
