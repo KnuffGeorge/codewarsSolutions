4ded81df945108434d6bf5f99f70701775d7c642
KnuffGeorge <georgethek@gmail.com>
2022-07-16 23:22:04 -0700

    number of occurrences in an element in array

--- codewars0716.js
+++ codewars0716.js
@@ -3,3 +3,9 @@
 function lowercaseCount(str){
     return [...str].filter(x=> x.charCodeAt(0) > 96 && x.charCodeAt(0) < 123).length
   }
+
+//   Write a function that returns the number of occurrences of an element in an array.
+
+Array.prototype.numberOfOccurrences = function(num) {
+    return this.filter(x => x === num).length;
+  }
