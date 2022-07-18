058567d2a58de286fb7b303f89ddd67f9dd4b95b
KnuffGeorge <georgethek@gmail.com>
2022-07-17 23:26:51 -0700

    peak array index

--- codewars0717.js
+++ codewars0717.js
@@ -30,3 +30,19 @@ function twoDecimalPlaces(number) {
     let arr = number.toString().split('.')
     return Number(arr[0] + "." + arr[1].slice(0,2))
   }
+
+//   Given an array of ints, return the index such that the sum of the elements to the right of that index equals the sum of the elements to the left of that index. If there is no such index, return -1. If there is more than one such index, return the left-most index.
+
+//   For example:
+  
+//   peak([1,2,3,5,3,2,1]) = 3, because the sum of the elements at indexes 0,1,2 == sum of elements at indexes 4,5,6. We don't sum index 3.
+//   peak([1,12,3,3,6,3,1]) = 2
+//   peak([10,20,30,40]) = -1
+  
+function peak(arr){
+    for (let i = 0; i < arr.length; i++){
+      if (arr.reduce((ac,el,ind)=> ind < i ? ac + el : ac, 0) === arr.reduce((ac,el,ind)=> ind > i ? ac + el : ac, 0)){
+      return i
+    }
+   }return -1
+   }
