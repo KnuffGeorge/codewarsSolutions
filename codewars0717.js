f775666584cad3113bbfec286205687eb853baca
KnuffGeorge <georgethek@gmail.com>
2022-07-17 23:24:58 -0700

    make password

--- codewars0717.js
+++ codewars0717.js
@@ -9,3 +9,15 @@ Array.prototype.remove_ = function(integer_list, values_list){
     return integer_list.filter(x=> !values_list.includes(x))
   
   }
+
+//   One suggestion to build a satisfactory password is to start with a memorable phrase or sentence and make a password by extracting the first letter of each word.
+
+//   Even better is to replace some of those letters with numbers (e.g., the letter O can be replaced with the number 0):
+  
+//       instead of including i or I put the number 1 in the password;
+//       instead of including o or O put the number 0 in the password;
+//       instead of including s or S put the number 5 in the password.
+
+function makePassword(phrase) {
+    return phrase.split(' ').map(x=> x[0] == "i" || x[0] == "I" ? "1" : x[0] == "o" || x[0] == "O" ? "0" : x[0] == "s" || x[0] == "S" ? 5 : x[0]).join('')
+  }
