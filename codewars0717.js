58d1d8927a2adb91b2c615c0cca4879f1f4b948f
KnuffGeorge <georgethek@gmail.com>
2022-07-17 23:25:53 -0700

    formatting decimal places

--- codewars0717.js
+++ codewars0717.js
@@ -21,3 +21,12 @@ Array.prototype.remove_ = function(integer_list, values_list){
 function makePassword(phrase) {
     return phrase.split(' ').map(x=> x[0] == "i" || x[0] == "I" ? "1" : x[0] == "o" || x[0] == "O" ? "0" : x[0] == "s" || x[0] == "S" ? 5 : x[0]).join('')
   }
+
+//   Each floating-point number should be formatted that only the first two decimal places are returned. You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+//   Don't round the numbers! Just cut them after two decimal places!
+
+function twoDecimalPlaces(number) {
+    let arr = number.toString().split('.')
+    return Number(arr[0] + "." + arr[1].slice(0,2))
+  }
