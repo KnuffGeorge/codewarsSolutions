eab4840087e27091e5a96d87fd5e89ea504f1da5
KnuffGeorge <georgethek@gmail.com>
2022-07-18 23:35:05 -0700

    make valley

--- codewars0718.js
+++ codewars0718.js
@@ -15,3 +15,27 @@ var vampire_test = function(a, b){
     let add = ("" + a + b).toString().split("").sort().join('') 
     return prod === add
    }
+
+
+
+//    Input : an array of integers.
+
+//    Output : this array, but sorted in such a way that there are two wings:
+
+//    the left wing with numbers decreasing,
+
+//    the right wing with numbers increasing.
+
+//    the two wings have the same length. If the length of the array is odd the wings are around the bottom, if the length is even the bottom is considered to be part of the right wing.
+
+//    each integer l of the left wing must be greater or equal to its counterpart r in the right wing, the difference l - r being as small as possible. In other words the right wing must be nearly as steep as the left wing.
+
+// The function is make_valley or makeValley or make-valley.
+
+function makeValley(arr) {
+    let sorted = arr.sort((a,b)=> b-a)
+    let left = []
+    let right = []
+    sorted.forEach((x,i) => i % 2 == 0 ? left.push(x) : right.push(x))
+    return left.concat(right.reverse())
+}
