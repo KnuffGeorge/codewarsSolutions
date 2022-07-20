2a33603ad86fdf9755e194248feb818a6685a51c
KnuffGeorge <georgethek@gmail.com>
2022-07-19 23:42:44 -0700

    phoenetic alphabet translation

--- codewars0719.js
+++ codewars0719.js
@@ -104,3 +104,34 @@ function pairs(ar){
     return count
   };
 
+//   You'll have to translate a string to Pilot's alphabet (NATO phonetic alphabet).
+
+// Input:
+
+// If, you can read?
+
+// Output:
+
+// India Foxtrot , Yankee Oscar Uniform Charlie Alfa November Romeo Echo Alfa Delta ?
+
+// Note:
+
+//     There are preloaded dictionary you can use, named NATO
+//     The set of used punctuation is ,.!?.
+//     Punctuation should be kept in your return string, but spaces should not.
+//     Xray should not have a dash within.
+//     Every word and punctuation mark should be seperated by a space ' '.
+//     There should be no trailing whitespace
+
+function to_nato(words) {
+    let answer = ""
+    let punc = ",.!?"
+    for (let i = 0; i<words.length; i++){
+      if(punc.includes(words[i])) answer += words[i] + " "
+      else if (words[i] == " ") answer += ""
+      else answer += NATO[words.toLowerCase()[i]] + " " 
+    }
+    return answer.trim()
+  }
+
+
