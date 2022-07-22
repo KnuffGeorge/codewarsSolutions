79b333b611e0b57257d9fff9e8e6da0ac6873f18
KnuffGeorge <georgethek@gmail.com>
2022-07-21 23:51:03 -0700

    sum consecutives

--- codewars0721.js
+++ codewars0721.js
@@ -50,3 +50,23 @@ function multi(arr) {
   function reverse(str) {
     return [...str].reverse().join('');
   }
+
+//   You are given a list/array which contains only integers (positive and negative). Your job is to sum only the numbers that are the same and consecutive. The result should be one list.
+
+// Extra credit if you solve it in one line. You can assume there is never an empty list/array and there will always be an integer.
+
+// Same meaning: 1 == 1
+
+// 1 != -1
+
+function sumConsecutives(s) {
+    let sum = 0
+    let answer = []
+    for (let i = 0; i<s.length; i++){
+      sum += s[i]
+      if (s[i] !== s[i+1]){
+        answer.push(sum)
+        sum = 0
+      } 
+    }return answer
+ }
