02b538ca61ba1f48845d3a39e2955d6d7902e6b7
KnuffGeorge <georgethek@gmail.com>
2022-08-16 12:27:06 -0700

    devs from all age groups

--- codewars0816.js
+++ codewars0816.js
@@ -55,3 +55,44 @@ function allContinents(list) {
 function findOddNames(list) {
     return list.filter(x=> [...x.firstName].reduce((ac,el)=> ac + el.charCodeAt(),0) % 2 == 1)
   }
+
+
+//   You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Your task is to return:
+
+//     true if developers from all of the following age groups have signed up: teens, twenties, thirties, forties, fifties, sixties, seventies, eighties, nineties, centenarian (at least 100 years young).
+//     false otherwise.
+
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 19, language: 'Python' },
+//   { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 29, language: 'JavaScript' },
+//   { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 39, language: 'Ruby' },
+//   { firstName: 'Noa', lastName: 'A.', country: 'Israel', continent: 'Asia', age: 40, language: 'Ruby' },
+//   { firstName: 'Andrei', lastName: 'E.', country: 'Romania', continent: 'Europe', age: 59, language: 'C' },
+//   { firstName: 'Maria', lastName: 'S.', country: 'Peru', continent: 'Americas', age: 60, language: 'C' },
+//   { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 75, language: 'Python' },
+//   { firstName: 'Chloe', lastName: 'K.', country: 'Guernsey', continent: 'Europe', age: 88, language: 'Ruby' },
+//   { firstName: 'Viktoria', lastName: 'W.', country: 'Bulgaria', continent: 'Europe', age: 98, language: 'PHP' },
+//   { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'JavaScript' }
+// ];
+
+// your function should return true as there is at least one developer from each age group.
+
+// Notes:
+
+//     The input array will always be valid and formatted as in the example above.
+//     Age is represented by a number which can be any positive integer up to 199.
+
+function isAgeDiverse(list) {
+    let ages = list.map(x=> x.age)
+    for (let i = 10; i < 100; i+=10){
+      if(!ages.some(x=> x>=i && x<i+10)){
+        return false
+      }if(!ages.some(x=> x >= 100)){
+        return false
+      }
+    }return true
+  }
