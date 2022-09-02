7d5e347e55bfe386132ac39145d62847362ff856
KnuffGeorge <georgethek@gmail.com>
2022-09-02 09:31:13 -0700

    histogram

---
+++ codewars0902.js
@@ -0,0 +1,26 @@
+// Background
+
+// A 6-sided die is rolled a number of times and the results are plotted as a character-based histogram.
+
+// Example:
+
+// 6|##### 5
+// 5|
+// 4|# 1
+// 3|########## 10
+// 2|### 3
+// 1|####### 7
+
+// Task
+
+// You will be passed the dice value frequencies, and your task is to write the code to return a string representing a histogram, so that when it is printed it has the same format as the example.
+// Notes
+
+//     There are no trailing spaces on the lines
+//     All lines (including the last) end with a newline \n
+//     A count is displayed beside each bar except where the count is 0
+//     The number of rolls may vary but there are never more than 100
+
+function histogram(results) {
+    return `6|${results[5] ? "#".repeat(results[5]) + " " : ""}${results[5] ? results[5] : ""}\n5|${results[4] ? "#".repeat(results[4]) + " " : ""}${results[4] ? results[4] : ""}\n4|${results[3] ? "#".repeat(results[3]) + " " : ""}${results[3] ? results[3] : ""}\n3|${results[2] ? "#".repeat(results[2]) + " "  : ""}${results[2] ? results[2] : ""}\n2|${results[1] ? "#".repeat(results[1]) + " "  : ""}${results[1] ? results[1] : ""}\n1|${results[0] ? "#".repeat(results[0]) + " "  : ""}${results[0] ? results[0] : ""}\n`
+    }
