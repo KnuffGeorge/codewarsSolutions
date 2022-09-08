5bafc07f947e2aa1759d24cf86bfc5da3a4e6e5c
KnuffGeorge <georgethek@gmail.com>
2022-09-08 11:02:56 -0700

    name capping

--- codewars0908.js
+++ codewars0908.js
@@ -72,3 +72,15 @@ function fruit(reels, spins){
     if (result.filter(x=> x == "Wild").length == 1) wild = 2
     return score[double] * wild
     }
+
+    // Create a method that accepts an array of names, and returns an array of each name with its first letter capitalized.
+
+    // example
+    
+    // capMe(['jo', 'nelson', 'jurie'])     // returns ['Jo', 'Nelson', 'Jurie']
+    // capMe(['KARLY', 'DANIEL', 'KELSEY']) // returns ['Karly', 'Daniel', 'Kelsey']
+
+    function capMe(names) {
+        return names.map(x => x[0].toUpperCase()+x.slice(1).toLowerCase());
+      }
+    
