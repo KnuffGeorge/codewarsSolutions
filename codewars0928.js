d046aa55b301de19edb7ab254a083392f7e8921e
KnuffGeorge <georgethek@gmail.com>
2022-09-28 16:06:37 -0700

    xbonnaci

---
+++ codewars0928.js
@@ -0,0 +1,46 @@
+// Number is a palindrome if it is equal to the number with digits in reversed order. For example, 5, 44, 171, 4884 are palindromes, and 43, 194, 4773 are not.
+
+// Write a function which takes a positive integer and returns the number of special steps needed to obtain a palindrome. The special step is: "reverse the digits, and add to the original number". If the resulting number is not a palindrome, repeat the procedure with the sum until the resulting number is a palindrome.
+
+// If the input number is already a palindrome, the number of steps is 0.
+
+// All inputs are guaranteed to have a final palindrome which does not overflow MAX_SAFE_INTEGER.
+// Example
+
+// For example, start with 87:
+
+//   87 +   78 =  165     - step 1, not a palindrome
+//  165 +  561 =  726     - step 2, not a palindrome
+//  726 +  627 = 1353     - step 3, not a palindrome
+// 1353 + 3531 = 4884     - step 4, palindrome!
+
+// 4884 is a palindrome and we needed 4 steps to obtain it, so answer for 87 is 4.
+
+var palindromeChainLength = function(n) {
+    const palindromeCheck = (n) => n.toString().split("").reverse().join("") == n.toString()
+    const specialStep = (n) => n + Number(n.toString().split("").reverse().join(''))
+    let stepCount = 0
+    while (!palindromeCheck(n)){
+      n = specialStep(n)
+      stepCount++
+    }return stepCount
+  };
+
+//   If you have completed the Tribonacci sequence kata, you would know by now that mister Fibonacci has at least a bigger brother. If not, give it a quick look to get how things work.
+
+// Well, time to expand the family a little more: think of a Quadribonacci starting with a signature of 4 elements and each following element is the sum of the 4 previous, a Pentabonacci (well Cinquebonacci would probably sound a bit more italian, but it would also sound really awful) with a signature of 5 elements and each following element is the sum of the 5 previous, and so on.
+
+// Well, guess what? You have to build a Xbonacci function that takes a signature of X elements - and remember each next element is the sum of the last X elements - and returns the first n elements of the so seeded sequence.
+
+// xbonacci {1,1,1,1} 10 = {1,1,1,1,4,7,13,25,49,94}
+// xbonacci {0,0,0,0,1} 10 = {0,0,0,0,1,1,2,4,8,16}
+// xbonacci {1,0,0,0,0,0,1} 10 = {1,0,0,0,0,0,1,2,3,6}
+// xbonacci {1,1} produces the Fibonacci sequence
+
+function Xbonacci(signature,n){
+  let factor = signature.length
+  let arr = signature.slice(0,n)
+  while (arr.length < n){
+    arr.push(arr.slice(-factor).reduce((ac,el)=> ac + el,0))
+  }return arr
+}
