31c6011579c3df74e46796843511b3e35fe65837
KnuffGeorge <georgethek@gmail.com>
2022-10-09 15:04:13 -0700

    caffeine script

--- codewars1009.js
+++ codewars1009.js
@@ -12,3 +12,31 @@
 function tea42(input) {
     return [...input.toString()].map(x=> x == 2 ? "t" : x).join("")
   };
+
+//   Complete the function which takes a non-zero integer as its argument.
+
+// If the integer is divisible by 3, return the string "Java".
+
+// If the integer is divisible by 3 and divisible by 4, return the string "Coffee"
+
+// If one of the condition above is true and the integer is even, add "Script" to the end of the string.
+
+// If none of the condition is true, return the string "mocha_missing!"
+// Examples
+
+// 1   -->  "mocha_missing!"
+// 3   -->  "Java"
+// 6   -->  "JavaScript"
+// 12  -->  "CoffeeScript"
+
+function caffeineBuzz(n){
+    let str = ""
+    if (n % 3 == 0){
+      str = "Java"
+    }if (n % 3 == 0 && n % 4 == 0){
+      str = "Coffee"
+    }if (str && n % 2 == 0){
+      str += "Script"
+    }if (!str) return "mocha_missing!"
+    return str
+    }
