76420bd77fdf5ca37a08221d38ecf81a8975e9ff
KnuffGeorge <georgethek@gmail.com>
2022-10-21 16:04:23 -0700

    ip addresses

---
+++ codewars1021.js
@@ -0,0 +1,22 @@
+// Implement a function that receives two IPv4 addresses, and returns the number of addresses between them (including the first one, excluding the last one).
+
+// All inputs will be valid IPv4 addresses in the form of strings. The last address will always be greater than the first one.
+// Examples
+
+// * With input "10.0.0.0", "10.0.0.50"  => return   50 
+// * With input "10.0.0.0", "10.0.1.0"   => return  256 
+// * With input "20.0.0.10", "20.0.1.0"  => return  246
+
+function ipsBetween(start, end){
+    let startArr = start.split(".").reverse()
+    let endArr = end.split(".").reverse()
+    let multiplier = 1
+    let diff = 0
+    endArr.forEach((x,i)=> {
+      diff += (x - startArr[i]) * multiplier
+      multiplier *= 256
+    })
+    return diff
+  }
+  
+  
