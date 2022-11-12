ece97a9a3ab8d3304d86204526ed69b12b123360
KnuffGeorge <georgethek@gmail.com>
2022-11-12 13:05:11 -0800

    dart score calculator

---
+++ codewars1112.js
@@ -0,0 +1,21 @@
+// You've just recently been hired to calculate scores for a Dart Board game!
+
+// Scoring specifications:
+
+//     0 points - radius above 10
+//     5 points - radius between 5 and 10 inclusive
+//     10 points - radius less than 5
+
+// If all radii are less than 5, award 100 BONUS POINTS!
+
+// Write a function that accepts an array of radii (can be integers and/or floats), and returns a total score using the above specification.
+
+// An empty array should return 0.
+
+function scoreThrows(radii){
+    if (radii.length == 0) return 0
+    let score = radii.reduce((ac,el)=> el < 5 ? ac += 10 : el <= 10 ? ac += 5 : ac, 0)
+    if (radii.every(x=> x<5)) score += 100
+    return score
+  }
+  
